9826421f42dba8530b51b62a046c3b45d58ec665
VoidProtocol <mw.orchowski@gmail.com>
2020-09-12 11:45:32 +0200

    Outrun-00: Simple scene created, graphics and meshes added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Config:")]
+    [SerializeField] private float _carSpeed;
+
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.AddForce(new Vector3(0.0f, 0.0f, _carSpeed) * Time.deltaTime);
+    }
+}
